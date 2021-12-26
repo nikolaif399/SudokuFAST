2732a63da086b9d65a87035dfdfed4adc28fda56
Nikolai Flowers <nikolaif399@gmail.com>
2021-12-25 18:22:43 -0800

    Rename cpp with cc and switch to google testing

--- test/sudoku_test.cc
+++ test/sudoku_test.cc
@@ -1,10 +1,9 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include <catch2/catch.hpp>
 #include <sudoku_solver.h>
 #include <sudoku_utils.h>
 
-TEST_CASE( "9x9 Sudoku can be solved", "[solver]" ) {
+#include <gtest/gtest.h>
+
+TEST(BasicSolver, NineXNine ) {
   bool use_diag = false;
   SudokuSolver solver(use_diag);
   board_t board_in = create_sudoku_board(9);
@@ -12,11 +11,11 @@ TEST_CASE( "9x9 Sudoku can be solved", "[solver]" ) {
   bool success = solver.solve();
   board_t board_out = solver.get_board();
 
-  REQUIRE(success);
-  REQUIRE(verify_sudoku_board(board_out,use_diag));
+  EXPECT_TRUE(success);
+  EXPECT_TRUE(verify_sudoku_board(board_out,use_diag));
 } 
 
-TEST_CASE( "9x9 SudokuX can be solved", "[solver]" ) {
+TEST(BasicSolver, NineXNineDiag ) {
   bool use_diag = true;
   SudokuSolver solver(use_diag);
   board_t board_in = create_sudoku_board(9);
@@ -24,11 +23,11 @@ TEST_CASE( "9x9 SudokuX can be solved", "[solver]" ) {
   bool success = solver.solve();
   board_t board_out = solver.get_board();
 
-  REQUIRE(success);
-  REQUIRE(verify_sudoku_board(board_out, use_diag));
+  EXPECT_TRUE(success);
+  EXPECT_TRUE(verify_sudoku_board(board_out,use_diag));
 }
 
-TEST_CASE( "4x4 Sudoku can be solved", "[solver]" ) {
+TEST(BasicSolver, FourXFour ) {
   bool use_diag = false;
   SudokuSolver solver(use_diag);
   board_t board_in = create_sudoku_board(4);
@@ -36,11 +35,11 @@ TEST_CASE( "4x4 Sudoku can be solved", "[solver]" ) {
   bool success = solver.solve();
   board_t board_out = solver.get_board();
 
-  REQUIRE(success);
-  REQUIRE(verify_sudoku_board(board_out, use_diag));
+  EXPECT_TRUE(success);
+  EXPECT_TRUE(verify_sudoku_board(board_out, use_diag));
 }
 
-TEST_CASE( "4x4 SudokuX can be solved", "[solver]" ) {
+TEST(BasicSolver, FourXFourDiag ) {
   bool use_diag = true;
   SudokuSolver solver(use_diag);
   board_t board_in = create_sudoku_board(4);
@@ -48,6 +47,6 @@ TEST_CASE( "4x4 SudokuX can be solved", "[solver]" ) {
   bool success = solver.solve();
   board_t board_out = solver.get_board();
   
-  REQUIRE(success);
-  REQUIRE(verify_sudoku_board(board_out, use_diag));
+  EXPECT_TRUE(success);
+  EXPECT_TRUE(verify_sudoku_board(board_out, use_diag));
 }
